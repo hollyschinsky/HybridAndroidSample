a95a6d2f6402efade4e0e52cdbb29c0c69e9434e
Holly Schinsky <hollyschinsky@gmail.com>
2015-06-18 22:14:16 -0400

    Initial Commit

---
+++ HybridAndroidSample/www/js/index.js
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        document.addEventListener("backbutton",this. exitApp, false);
+        window.plugins.toast.showLongBottom('Use the back button to return to main.');
+        app.receivedEvent('deviceready');
+    },
+    exitCordova: function(){
+        navigator.app.exitApp();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+    showToast: function(){
+        window.plugins.toast.showShortCenter('PHONEGAP IS AWESOME!!!');
+    },
+
+    showDeviceInfo: function(){
+        var message = 'Cordova version: ' + device.cordova;
+        message += '\n\nDevice Model: ' + device.model;
+        message += '\n\nDevice Version (Android): ' + device.version;
+        alert(message);
+    },
+
+    openWeb: function(){
+        var url = "http://phonegap.com"
+        window.open(url)
+    }
+};
+
+app.initialize();
